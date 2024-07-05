acf3bbc3ad3003abed354d24799ca2a0e6de9755
Lokasku <lukasku@proton.me>
2024-07-05 20:39:10 +0200

    Save

--- stlc/src/main.rs
+++ stlc/src/main.rs
@@ -2,14 +2,14 @@
 use chumsky::Parser;
 mod ast;
 mod parser;
+mod typechecker;
 
 fn main() {
     let src = std::fs::read_to_string(std::env::args().nth(1).unwrap()).unwrap();
-
-    println!("\n\n\n\n{}\n\n\n\n", src);
-
     match parser::parser().parse(src.clone()) {
         Ok(ast) => println!("{:#?}", ast),
-        Err(parse_error) => parse_error.into_iter().for_each(|e| println!("{:?}", e)),
+        Err(parse_error) => parse_error
+            .into_iter()
+            .for_each(|e| println!("{:?}", e.reason(),)),
     }
 }
